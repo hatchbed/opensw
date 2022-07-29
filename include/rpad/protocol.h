d280f6a29aa915a74a3bdcaa4e2cef8d4d12ea18
Marc Alban <marcalban@hatchbed.com>
2022-07-28 21:51:11 -0500

    Add documentation and license headers. (#5)

--- include/rpad/protocol.h
+++ include/rpad/protocol.h
@@ -1,3 +1,34 @@
+/**
+ * Copyright (c) 2022, Hatchbed
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #pragma once
 
 #include <array>
@@ -25,8 +56,46 @@ const std::string CMD_GET_ROBOT_HEALTH = "getrobothealth";
 const std::string CMD_GET_SPD_VERSION = "getsdpversion";
 const std::string CMD_GET_SYSTEM_RESOURCE = "getsystemresource";
 
-
+/**
+ * Inflate encoded binary data.
+ *
+ * First the data is decoded from a base64 string and then it is processed to
+ * decode from run length encoding.
+ *
+ * A 9 byte buffer in the decoded base64 data is assumed, which should contain
+ * the specifics of the run length encoding:
+ *
+ *   [0] 'R'
+ *   [1] 'L'
+ *   [2] 'E'
+ *   [3] sentinel byte 1 (usually 129)
+ *   [4] sentinel byte 2 (usually 127)
+ *   [5 - 8] length of inflated data (uint32)
+ *
+ * @param[in] base64_data  The base64 encoded data string.
+ *
+ * @returns The inflated data if successful, empty otherwise.
+ */
 std::vector<uint8_t> inflate(const std::string& base64_data);
+
+/**
+ * Parse laser points from base64 encoded laser point data.
+ *
+ * See documentation of `inflate` function.
+ *
+ * After the base64 encoded data is decoded and inflated, the memory layout of
+ * the data is 12 bytes per point:
+ *
+ *    [0 - 3] distance (float)
+ *    [4 - 7] angle (float)
+ *    [8] valid (uint8)
+ *    [9 - 11] padding
+ *
+ * @param[in] base64_data  The base64 encoded data string.
+ * @param[in] fill_gaps  Whether or not to fill in any single point gaps with 'invalid' points.
+ *
+ * @returns Parsed laser points, sorted by ascending angle, if successful, empty otherwise.
+ */
 std::vector<LaserPoint> parseLaserPoints(const std::string& base64_data, bool fill_gaps=false);
 
 }  // namespace rpad
--- src/client.cpp
+++ src/client.cpp
@@ -1,3 +1,34 @@
+/**
+ * Copyright (c) 2022, Hatchbed
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <rpad/client.h>
 
 #include <chrono>
@@ -68,23 +99,6 @@ bool Client::connected() const {
 
 void Client::disconnect() {
     socket_.close();
-}
-
-std::optional<std::string> Client::getSdpVersion() {
-    msg::SimpleRequest req;
-    req.request_id = id_dist_(rng_);
-    req.command = CMD_GET_SPD_VERSION;
-    auto resp = sendAndReceive(req.dump());
-    if (!resp) {
-        return {};
-    }
-    auto response = msg::GetSdpVersionResponse::fromJson(*resp);
-    if (!response) {
-        error("Failed to parse response");
-        return {};
-    }
-
-    return response->result.sdp_version;
 }
 
 std::optional<int> Client::getBatteryPercentage() {
@@ -376,6 +390,23 @@ std::optional<ImuData> Client::getRawImuData() {
     imu_data.orientation = { response->result.quaternion.w, response->result.quaternion.x, response->result.quaternion.y, response->result.quaternion.z };
 
     return imu_data;
+}
+
+std::optional<std::string> Client::getSdpVersion() {
+    msg::SimpleRequest req;
+    req.request_id = id_dist_(rng_);
+    req.command = CMD_GET_SPD_VERSION;
+    auto resp = sendAndReceive(req.dump());
+    if (!resp) {
+        return {};
+    }
+    auto response = msg::GetSdpVersionResponse::fromJson(*resp);
+    if (!response) {
+        error("Failed to parse response");
+        return {};
+    }
+
+    return response->result.sdp_version;
 }
 
 std::optional<std::string> Client::sendAndReceive(const std::string& msg) {
