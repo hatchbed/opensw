620094f64fed85bf22cb8b279388de21d0da77e4
Marc Alban <marcalban@hatchbed.com>
2022-07-28 21:49:10 -0500

    Add documentation.

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # rpad
-Open client SDK for communicating with SlamTec Slamware devices such as the M2M1 mapper.
+Open source client SDK for communicating with SlamTec Slamware devices such as
+the [M2M1 mapper](https://www.slamtec.com/en/Lidar/Mapper) over ethernet.
+
+An official SDK is [available](https://www.slamtec.com/en/Support#rplidar-mapper),
+but appears to be closed source.
+
+Note: For the standalone SlamTec lidar devices, an existing open source driver
+is already available: [rplidar](https://github.com/Slamtec/rplidar_ros), but
+this uses a different protocol than the mapper devices.
+
+Currently, the interface is only partially implemented to expose the lidar scan
+and IMU data as well as some of the mapping data.
+
+More of the interface provided by the official SDK can be easily added, as
+needed, since the protocol is straightforward JSON over TCP.  These additional
+interfaces appear to support their robotic platforms, which use the same
+protocol.
+
+## Build Dependencies
+
+- [asio](https://think-async.com/Asio/) for TCP communication
+- [asyncapi_gencpp](https://github.com/hatchbed/asyncapi_gencpp) for json 
+    protocol serialization/deserialization code generation from an
+    [asyncapi](https://www.asyncapi.com/) style spec file.
+- [eigen](https://eigen.tuxfamily.org/) for some datatypes
+- [gtest](https://github.com/google/googletest) for unit testing
+- [opencv](https://opencv.org/) for some datatypes
+- [spdlog](https://github.com/gabime/spdlog) for formatted logging
+
+## Supported Commands
+
+- `GetImuInRobotCoordinate`: Gets IMU sensor data
+- `GetKnownArea`: Gets the current bounds of a map
+- `GetLaserScan`: Gets laser scan data
+- `GetLocation`: Gets the current location estimate
+- `GetMapData`: Gets raw map data
+- `GetPose`: Gets the current pose estimate
+- `GetRobotHealth`: Gets device health status data
+- `GetSpdVersion`: Gets version information
+- `GetSystemResource`: Gets system information
+
+## Example Usage:
+```
+#include <rpad/client.h>
+
+int main(int argc, char **argv) {
+
+  rpad::Client client;
+  if (client.connect(host, port, 500)) {
+    auto scan = client.getLaserScan();
+  }
+}
+
+```
+
+## ROS Support
+
+This library is agnostic to ROS, but is packaged to work in a ROS1 or ROS2
+workspace.
+
+See [rpad_ros] for a simple ROS laser scan and IMU driver built by wrapping this
+SDK.
--- include/rpad/client.h
+++ include/rpad/client.h
@@ -1,10 +1,40 @@
+/**
+ * Copyright (c) 2022, Hatchbed
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #pragma once
 
 #include <optional>
 #include <random>
 #include <string>
 #include <system_error>
-
 
 #include <asio.hpp>
 #include <Eigen/Core>
@@ -14,28 +44,154 @@
 
 namespace rpad {
 
+/**
+ * Primary class used to communicate with device.
+ */
 class Client {
   public:
     Client();
     ~Client() = default;
 
+    /**
+     * Connect to device at host:port over TCP.
+     *
+     * @param[in] host  The hostname or IP address of the device.
+     * @param[in] port  The port to connect to.
+     * @param[in] timeout_ms  Connection timeout in milliseconds.
+     *
+     * @returns True if the connection is successful, false otherwise.
+     */
     bool connect(const std::string& host, int port, int timeout_ms);
+
+    /**
+     * Check if the device is currently connected.
+     *
+     * @returns True if the device is connected, false otherwise.
+     */
     bool connected() const;
+
+    /**
+     * Disconnect from the device.
+     */
     void disconnect();
 
+    /**
+     * Get the current device batter percentage from device.
+     *
+     * Only applies to devices with a battery.
+     *
+     * @returns The battery percentage if the request is successfull, empty otherwise.
+     */
     std::optional<int> getBatteryPercentage();
+
+    /**
+     * Get map data layer as a bitmap from device.
+     *
+     * @param[in] x  The min x extent of the map in meters.
+     * @param[in] y  The min y extent of the map in meters.
+     * @param[in] width  The width of the map in meters.
+     * @param[in] heigher  The height of the map in meters.
+     * @param[in] kind  The map layer to retrieve.
+     *
+     * @returns The map data layer if the request is successful, null otherwise.
+     */
     Bitmap::Ptr getBitmapData(float x, float y, float width, float height, MapKind kind);
+
+    /**
+     * Get the current device board temperature.
+     *
+     * @returns The board temperature if the request is successfull, empty otherwise.
+     */
     std::optional<int> getBoardTemperature();
+
+    /**
+     * Get whether or not DC IN is connected from device.
+     *
+     * Only applies to devices with a battery.
+     *
+     * @returns Whether or not DC IN is connected if the request is successful, empty otherwise.
+     */
     std::optional<int> getDcInConnected();
+
+    /**
+     * Get health status information from device.
+     *
+     * @returns Health status information if the request is successful, empty otherwise.
+     */
     std::optional<HealthStatus> getHealthStatus();
+
+    /**
+     * Get IMU data from device.
+     *
+     * Note: currently only the orientation fields appear to be filled in.
+     *
+     * @returns IMU data if the request is successful, empty otherwise.
+     */
     std::optional<ImuData> getImuData();
+
+    /**
+     * Get whether or not the battery is charging from device.
+     *
+     * Only applies to devices with a battery.
+     *
+     * @returns Whether or not the battery is charging if the request is successful, empty otherwise.
+     */
     std::optional<int> getIsCharging();
+
+    /**
+     * Get the current extents of a map data layer from the device.
+     *
+     * @param[in] type  The type of map.
+     * @param[in] kind  The map layer to get the extents of.
+     *
+     * @returns The map layer extents if the request is successful, empty otherwise.
+     */
     std::optional<cv::Rect_<float>> getKnownArea(MapType type, MapKind kind);
+
+    /**
+     * Get laser scan data from device.
+     *
+     * @returns Laser scan data if the request is successful, empty otherwise.
+     */
     LaserScan::Ptr getLaserScan();
+
+    /**
+     * Get location estimate from device.
+     *
+     * @returns Location estimate if the request is successful, empty otherwise.
+     */
     std::optional<Eigen::Vector3d> getLocation();
+
+    /**
+     * Get whether or not the device is on the dock from device.
+     *
+     * Only applies to devices with a dock.
+     *
+     * @returns Whether or not the device is on the dock if the request is successful, empty otherwise.
+     */
     std::optional<int> getOnDock();
+
+    /**
+     * Get pose estimate from device.
+     *
+     * @returns Pose estimate if the request is successful, empty otherwise.
+     */
     std::optional<Eigen::Isometry3d> getPose();
+
+    /**
+     * Get IMU data from device.
+     *
+     * Note: currently only the orientation fields appear to be filled in.
+     *
+     * @returns IMU data if the request is successful, empty otherwise.
+     */
     std::optional<ImuData> getRawImuData();
+
+    /**
+     * Get the SPD version from the device.
+     *
+     * @returns The SPD version if the request is successful, empty otherwise.
+     */
     std::optional<std::string> getSdpVersion();
 
   private:
@@ -51,7 +207,7 @@ class Client {
     asio::streambuf stream_buffer_;
     std::mt19937 rng_;
     std::uniform_int_distribution<std::mt19937::result_type> id_dist_;
-    int timeout_ms_;
+    int timeout_ms_ = 500;
     bool connection_failed_ = false;
 };
 
